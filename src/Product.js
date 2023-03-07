5ab3d943792528222886629ef7df037b0af9a274
Nicole Ordoqui <nicole.ordoqui@gmail.com>
2023-03-06 23:35:07 -0300

    initial commit

---
+++ src/Product.js
@@ -0,0 +1,15 @@
+class Product {
+    constructor({id, title, description, price, thumbnail, code, stock}) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+
+    // todos los métodos que consideran que puedan ir acá.
+}
+
+module.exports = Product;
---
+++ src/errorMessage.js
@@ -0,0 +1,5 @@
+const errorMessage = {
+    NOT_FOUND: 'product not found',
+}
+
+module.exports = errorMessage;
